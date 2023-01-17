4ae778ad9b0faa5e4e8719d6f22865acb7da3d82
Abdulmajid Usman <abstractmajid@gmail.com>
2023-01-17 18:06:38 +0100

    core setup: add app colors

---
+++ lib/core/theme/app_color.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColor {
+  static const alabaster = Color(0XFFFCFCFC);
+  static const white = Color(0XFFFFFFFF);
+  static const matisse = Color(0XFF186F93);
+  static const waterloo = Color(0XFF7A809B);
+  static const stormGrey = Color(0XFF646B85);
+  static const rhino = Color(0XFF374072);
+  static const pastelPink = Color(0XFFFFDCE5);
+  static const health = Color(0XFF531423);
+  static const frenchPass = Color(0XFFD0F0FD);
+  static const wedgeWood = Color(0XFF477F96);
+  static const mineShaft = Color(0XFF262626);
+  static const steelBlue = Color(0XFF3F87A5);
+  static const curiousBlue = Color(0XFF319DC9);
+  static const eden = Color(0XFF0E465D);
+  static const catskillWhite = Color(0XFFE7F0F4);
+  static const bittersweet = Color(0XFFFD6B6D);
+  static const chamois = Color(0XFFEBD7A8);
+  static const goBen = Color(0XFF70654D);
+  static const easternBlue = Color(0XFF189393);
+  static const nepal = Color(0XFF91B2C0);
+}
