ea20f6ba0fbcc849c96a8d003e7f3c295d368f76
Abdulmajid Usman <abstractmajid@gmail.com>
2023-01-18 13:53:01 +0100

    fixes

--- lib/feature/business/view/screens/business_detail_screen.dart
+++ lib/feature/business/view/screens/business_detail_screen.dart
@@ -73,127 +73,131 @@ class BusinessDetailScreen extends StatelessWidget {
         ],
       ),
       backgroundColor: AppColor.alabaster,
-      body: ListView(
-        padding: const EdgeInsets.symmetric(
-            horizontal: kspace, vertical: kmediumSpace),
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(kTinyBorderRadius),
-            child: Container(
-              width: size.width,
-              alignment: Alignment.center,
-              child: Column(
-                children: [
-                  Container(
-                    width: size.width,
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: kmediumSpace, vertical: kLargeSpace),
-                    color: AppColor.chathamsBlue,
-                    alignment: Alignment.center,
-                    child: Column(
-                      children: [
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              padding: const EdgeInsets.all(12),
-                              decoration: BoxDecoration(
-                                color: AppColor.wedgeWood,
-                                borderRadius:
-                                    BorderRadius.circular(kTinyBorderRadius),
-                              ),
-                              alignment: Alignment.center,
-                              child: const Icon(
-                                Icons.business_center_outlined,
-                                color: AppColor.mischka,
-                              ),
-                            ),
-                            const XMargin(kspace),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Text(
-                                  'Paragraph Limited',
-                                  style: TextStyle(
-                                    color: AppColor.white,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.w500,
-                                  ),
+      body: ScrollConfiguration(
+        behavior: const ScrollBehavior(),
+        child: ListView(
+          padding: const EdgeInsets.symmetric(
+              horizontal: kspace, vertical: kmediumSpace),
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(kTinyBorderRadius),
+              child: Container(
+                width: size.width,
+                alignment: Alignment.center,
+                child: Column(
+                  children: [
+                    Container(
+                      width: size.width,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: kmediumSpace, vertical: kLargeSpace),
+                      color: AppColor.chathamsBlue,
+                      alignment: Alignment.center,
+                      child: Column(
+                        children: [
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.all(12),
+                                decoration: BoxDecoration(
+                                  color: AppColor.wedgeWood,
+                                  borderRadius:
+                                      BorderRadius.circular(kTinyBorderRadius),
                                 ),
-                                const YMargin(kspace),
-                                Container(
-                                  padding: const EdgeInsets.all(ksmallSpace),
-                                  decoration: BoxDecoration(
-                                    color: AppColor.frenchPass,
-                                    borderRadius:
-                                        BorderRadius.circular(kborderRadius),
-                                  ),
-                                  child: const Text(
-                                    'Informal sector',
+                                alignment: Alignment.center,
+                                child: const Icon(
+                                  Icons.business_center_outlined,
+                                  color: AppColor.mischka,
+                                ),
+                              ),
+                              const XMargin(kspace),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text(
+                                    'Paragraph Limited',
                                     style: TextStyle(
-                                      color: AppColor.mineShaft,
+                                      color: AppColor.white,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w500,
                                     ),
                                   ),
-                                ),
-                                const YMargin(kspace),
-                                const Text(
-                                  'ID:MCI-23-02-000001',
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    color: AppColor.mischka,
+                                  const YMargin(kspace),
+                                  Container(
+                                    padding: const EdgeInsets.all(ksmallSpace),
+                                    decoration: BoxDecoration(
+                                      color: AppColor.frenchPass,
+                                      borderRadius:
+                                          BorderRadius.circular(kborderRadius),
+                                    ),
+                                    child: const Text(
+                                      'Informal sector',
+                                      style: TextStyle(
+                                        color: AppColor.mineShaft,
+                                      ),
+                                    ),
                                   ),
-                                ),
-                              ],
-                            )
-                          ],
-                        )
-                      ],
+                                  const YMargin(kspace),
+                                  const Text(
+                                    'ID:MCI-23-02-000001',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      color: AppColor.mischka,
+                                    ),
+                                  ),
+                                ],
+                              )
+                            ],
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                  Container(
-                    width: size.width,
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: kmediumSpace, vertical: kspace),
-                    color: AppColor.white,
-                    alignment: Alignment.center,
-                    child: Row(
-                      children: [
-                        DottedBorder(
-                          color: AppColor.chathamsBlue,
-                          child: const Padding(
-                            padding: EdgeInsets.all(ktinyspace),
-                            child: Text(
-                              'General Information',
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: AppColor.chathamsBlue,
+                    Container(
+                      width: size.width,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: kmediumSpace, vertical: kspace),
+                      color: AppColor.white,
+                      alignment: Alignment.center,
+                      child: Row(
+                        children: [
+                          DottedBorder(
+                            color: AppColor.chathamsBlue,
+                            child: const Padding(
+                              padding: EdgeInsets.all(ktinyspace),
+                              child: Text(
+                                'General Information',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: AppColor.chathamsBlue,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        const XMargin(kspace),
-                        const Text(
-                          'Applications',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: AppColor.stormGrey,
+                          const XMargin(kspace),
+                          const Text(
+                            'Applications',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: AppColor.stormGrey,
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          const YMargin(kxtraLargeSpace),
-          const BusinessDetailTile(
-              title: 'Address', name: '14 Ogboko Street, Mgbuoba Obio-Apkor'),
-          const BusinessDetailTile(title: 'L.G.A', name: 'Obio-Apkor'),
-          const BusinessDetailTile(title: 'Ward', name: 'Obio-Apkor'),
-          const BusinessDetailTile(title: 'Phone', name: '08012345678'),
-          const BusinessDetailTile(title: 'Email', name: 'dummytest@gmail.com'),
-        ],
+            const YMargin(kxtraLargeSpace),
+            const BusinessDetailTile(
+                title: 'Address', name: '14 Ogboko Street, Mgbuoba Obio-Apkor'),
+            const BusinessDetailTile(title: 'L.G.A', name: 'Obio-Apkor'),
+            const BusinessDetailTile(title: 'Ward', name: 'Obio-Apkor'),
+            const BusinessDetailTile(title: 'Phone', name: '08012345678'),
+            const BusinessDetailTile(
+                title: 'Email', name: 'dummytest@gmail.com'),
+          ],
+        ),
       ),
     );
   }
